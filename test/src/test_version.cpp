6edacb5f78c56a2c423a332c0de620f424aff9cc
Pavel-Alyukaev <Pavel-Alyukaev@yandex.ru>
2023-01-19 22:36:12 +0700

    Revert "cmake"
    This reverts commit 205f8de0d2a34572583fe7a958ed12a6545fc8f3.

--- test/src/test_version.cpp
+++ test/src/test_version.cpp
@@ -2,18 +2,13 @@
 
 #include "lib.h"
 
-//#include <boost/test/unit_test.hpp>
-//
-//BOOST_AUTO_TEST_SUITE(test_version)
-//
-//BOOST_AUTO_TEST_CASE(test_valid_version)
-//{
-//    BOOST_CHECK(version() > 0);
-//}
-//
-//BOOST_AUTO_TEST_SUITE_END()
-int main(int, char**) {
-    /*std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;*/
-    return 0;
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    BOOST_CHECK(version() > 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
